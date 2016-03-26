36913d3a50169dba33f072052bdc96585880ba72
tsub <tsubasatakayama511@gmail.com>
2016-03-26 17:40:47 +0900

    writing ping_spec

--- spec/lita/handlers/ping_spec.rb
+++ spec/lita/handlers/ping_spec.rb
@@ -1,4 +1,10 @@
 require "spec_helper"
 
 describe Lita::Handlers::Ping, lita_handler: true do
+  it { is_expected.to route('ping') }
+  it { is_expected.to route('ping').to(:ping) }
+  it 'receiving pong' do
+    send_message 'ping'
+    expect(replies).to eq ['pong']
+  end
 end
